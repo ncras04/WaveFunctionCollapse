28f0af05034be48ad164bc49b5462fb998b5907c
ncras04 <nicolasaschmitt@googlemail.com>
2025-05-24 02:31:08 +0200

    v 1.0

---
+++ Program.cs
@@ -0,0 +1,208 @@
+﻿using System.Text;
+
+namespace WFC
+{
+    internal class Program
+    {
+        enum EDirection
+        {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+
+        private static readonly int[] Masks = [0b_1000_1000_1000_1000, 0b_0100_0100_0100_0100, 0b_0010_0010_0010_0010, 0b_0001_0001_0001_0001];
+
+        private const int NullTile = 0b_1111;
+        private static readonly int[] StreetTiles =
+            [
+            0b_0100_1011_0000, 0b_1000_0111_0000, 0b_0001_1110_0000, 0b_0010_1101_0000,
+            0b_0101_1010_0000, 0b_1001_0110_0000, 0b_0110_1001_0000, 0b_1010_0101_0000,
+            0b_0011_1100_0000, 0b_1100_0011_0000, 0b_0000_1111_0000,
+            0b_0111_1000_0000, 0b_1011_0100_0000, 0b_1101_0010_0000, 0b_1110_0001_0000,
+            0b_1111_0000_0000
+            ];
+
+
+        private const char NullChar = ' ';
+        private static readonly char[] StreetChars =
+            [
+            '╩', '╦', '╣', '╠',
+            '╝', '╗', '╚', '╔',
+            '║', '═', '╬',
+            '╨','╥','╡', '╞',
+            ' '
+            ];
+
+        private static int[,]? map;
+
+        private static Random rng = new Random();
+
+        static void Main(string[] args)
+        {
+            string[] size = args[0].Split(',');
+
+            if (!int.TryParse(size[0], out int sizeX) || !int.TryParse(size[1], out int sizeY))
+                return;
+
+            map = new int[sizeY, sizeX];
+
+            Dictionary<int, char> tiles = new Dictionary<int, char>
+            {
+                { NullTile, NullChar }
+            };
+
+            for (int i = 0; i < StreetTiles.Length; i++)
+            {
+                tiles.Add(StreetTiles[i], StreetChars[i]);
+            }
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            top:
+
+            for (int y = 0; y < sizeY; y++)
+            {
+                for (int x = 0; x < sizeX; x++)
+                {
+                    map[y, x] = NullTile;
+                }
+            }
+
+            map[rng.Next(sizeY - 1), rng.Next(sizeX - 1)] = 0b_1000_0111_0000;
+
+            while (true)
+            {
+                List<(int, int, int)> leastEntropyTiles = [(tiles.Count, -1, -1)];
+
+                for (int y = 0; y < sizeY; y++)
+                {
+                    for (int x = 0; x < sizeX; x++)
+                    {
+                        //check if collapsed
+                        if ((NullTile & map[y, x]) == 0)
+                            continue;
+
+                        //check up if not on y 0
+                        int tileUpCheck = 0;
+                        if (y != 0)
+                        {
+                            tileUpCheck = map[y - 1, x] << 1;
+                            tileUpCheck &= Masks[(int)EDirection.UP];
+                        }
+
+                        //check down if not on y max
+                        int tileDownCheck = 0;
+                        if (y != sizeY - 1)
+                        {
+                            tileDownCheck = map[y + 1, x] >> 1;
+                            tileDownCheck &= Masks[(int)EDirection.DOWN];
+                        }
+
+                        //check left if not on x 0 
+                        int tileLeftCheck = 0;
+                        if (x != 0)
+                        {
+                            tileLeftCheck = map[y, x - 1] << 1;
+                            tileLeftCheck &= Masks[(int)EDirection.LEFT];
+                        }
+
+                        //check right if not on x max
+                        int tileRightCheck = 0;
+                        if (x != sizeX - 1)
+                        {
+                            tileRightCheck = map[y, x + 1] >> 1;
+                            tileRightCheck &= Masks[(int)EDirection.RIGHT];
+                        }
+
+                        int result = tileUpCheck ^ tileDownCheck ^ tileLeftCheck ^ tileRightCheck;
+
+                        map[y, x] = result;
+
+                        result -= NullTile & result;
+
+                        int entropyNumber = 0;
+
+                        foreach (var tile in tiles)
+                        {
+                            if ((tile.Key & result) == result)
+                            {
+                                entropyNumber++;
+                            }
+                        }
+
+                        if (entropyNumber < leastEntropyTiles[0].Item1)
+                            leastEntropyTiles = [(entropyNumber, y, x)];
+                        else if (entropyNumber == leastEntropyTiles[0].Item1)
+                            leastEntropyTiles.Add((entropyNumber, y, x));
+                    }
+                }
+
+                if (leastEntropyTiles[0].Item2 == -1 || leastEntropyTiles[0].Item3 == -1)
+                    break;
+
+                if (leastEntropyTiles.Count > 1)
+                {
+                    int y = leastEntropyTiles[rng.Next(leastEntropyTiles.Count)].Item2;
+                    int x = leastEntropyTiles[rng.Next(leastEntropyTiles.Count)].Item3;
+
+                    leastEntropyTiles = [(0, y, x)];
+                }
+
+                var leastEntropyNumber = map[leastEntropyTiles[0].Item2, leastEntropyTiles[0].Item3];
+
+                leastEntropyNumber -= NullTile & leastEntropyNumber;
+
+                List<int> possibleTiles = new List<int>();
+
+                foreach (var tile in tiles)
+                {
+                    if ((tile.Key & leastEntropyNumber) == leastEntropyNumber)
+                    {
+                        possibleTiles.Add(tile.Key);
+                    }
+                }
+
+                int randomPick = NullTile;
+
+                while (randomPick == NullTile)
+                    randomPick = possibleTiles[rng.Next(possibleTiles.Count)];
+
+                map[leastEntropyTiles[0].Item2, leastEntropyTiles[0].Item3] = randomPick;
+
+                tiles.TryGetValue(randomPick, out char print);
+                Console.SetCursorPosition(leastEntropyTiles[0].Item3, leastEntropyTiles[0].Item2);
+                Console.Write(print);
+
+
+                //int blacks = 0;
+
+                //for (int y = 0; y < sizeY; y++)
+                //{
+                //    for (int x = 0; x < sizeX; x++)
+                //    {
+                //        if (tiles.TryGetValue(map[y, x], out char print))
+                //        {
+                //            int color = blacks + (1 + y) * x % 15;
+                //            if (color == 0)
+                //            if (color == 0)
+                //            {
+                //                color = 1;
+                //                blacks++;
+                //            }
+
+                //            Console.ForegroundColor = (ConsoleColor)color;
+                //            Console.Write(print);
+                //        }
+                //        else
+                //            Console.Write(NullChar);
+                //    }
+                //    Console.Write('\n');
+                //}
+            }
+            goto top;
+        }
+    }
+}
